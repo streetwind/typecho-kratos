bd0fd093ec34d652127ff6120983dd8b59c776f5
wind <mofeng64@gmail.com>
2021-04-15 01:38:38 +0800

    index footer

--- footer.php
+++ footer.php
@@ -1,14 +1,18 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-        </div><!-- end .row -->
-    </div>
+
+</div><!-- end .row -->
+</div>
 </div><!-- end #body -->
 
-<footer id="footer" role="contentinfo">
-    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-    <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
-</footer><!-- end #footer -->
+<div class="k-footer">
+    <div class="f-toolbox">
+    </div>
+    <div class="container">
+    </div>
+</div>
 
 <?php $this->footer(); ?>
 </body>
+
 </html>
